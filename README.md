f51c5f4c50b5bf0f43ab24e1f4ba550d3daa42a2
Rachit Nag <rachitknag@gmail.com>
2026-09-17 10:35:17 +0530

    solved Print Each Array Element with Its Index

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # edge-case-lab
 Daily experiments with Data Structures &amp; Algorithms in Java — solving problems, learning patterns, optimizing solutions, and documenting the edge cases that try to break them.
+
+# Array Basics:
+1. Print Each Array Element with Its Index
---
+++ array-basics/Problem1.java
@@ -0,0 +1,22 @@
+// Print Each Array Element with Its Index
+
+import java.util.Scanner;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        System.out.println("Enter arr elments: ");
+        for(int i=0;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("array elements are:\n ele : index");
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]+" : "+i);
+        }
+
+        sc.close();
+    }
+}
